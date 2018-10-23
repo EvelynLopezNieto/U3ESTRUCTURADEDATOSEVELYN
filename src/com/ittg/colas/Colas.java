050f771018bb03fcacd6b2f636bdd70f7c5d0754
Evelyn Lopez Nieto <ingnieto01@gmail.com>
2018-10-23 18:17:38 -0500

    Se agrego los metodos crearCola(), llena(), vacia()

--- src/com/ittg/colas/Colas.java
+++ src/com/ittg/colas/Colas.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 
 public class Colas implements IColas {
     
-    private int tam=5;
+    private int tam;
     private int frente;
     private int fin;
     private int[] cola;
@@ -13,13 +13,17 @@ public class Colas implements IColas {
     public Colas() {
         frente=0;
         fin=-1;
-        cola=new int[tam];
+        
+    }
+    
+    public void crearCola(){
+        cola=new int[Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tamaño de su cola"))];
     }
     
     @Override
     public void insertar() {
         
-        if(fin==tam-1){
+        if(fin==cola.length-1){
             JOptionPane.showMessageDialog(null,"La cola esta llena");
         }
         else{
@@ -32,7 +36,7 @@ public class Colas implements IColas {
     public void imprimir() {
     
         if(!(fin==-1)){
-            System.out.println("La cola de tamaño: "+tam+" contiene "+(fin+1)+" elementos");
+            System.out.println("La cola de tamaño "+cola.length+" contiene "+(fin+1)+" elementos");
         for(int i=frente; i<=fin; i++){
         
            System.out.println("Elemento ["+i+"]="+cola[i]);
@@ -42,16 +46,50 @@ public class Colas implements IColas {
             JOptionPane.showMessageDialog(null,"Cola vacia");
         }
     }
+    
     @Override
     public void quitar() {
-        if(!(fin==tam-1)){
-            frente++;
+        //!(frente-1>0):frente++
+        if(!(fin==-1)){
+            for(int j=frente; j<=fin-1; j++){
+                cola[j]=cola[j+1];
+            }
+            fin--;
+            
             JOptionPane.showMessageDialog(null,"Elemento eliminado");
         }
         else{
             JOptionPane.showMessageDialog(null,"No hay datos para eliminar");
         }
+                
+    }
         
+    @Override
+    public void llena(){
+        if(!estaVacia()){
+            JOptionPane.showMessageDialog(null,"Hay "+(fin+1)+" elementos en cola");
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"La cola no tiene ningun elemento...");
+        }
+        
+    }
+    
+    public boolean estaVacia(){
+        if(fin==-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    @Override
+    public void vacia(){
+        if(estaVacia()){
+            JOptionPane.showMessageDialog(null,"La cola esta vacia");
+        }else{
+            JOptionPane.showMessageDialog(null,"La cola tiene elementos... ingrese a la\n"
+                    + "opcion de esta llena para ver cuantos datos tiene.");
+        }
     }
     
     @Override
@@ -60,26 +98,29 @@ public class Colas implements IColas {
         
         do{
         int opcion=Integer.parseInt(JOptionPane.showInputDialog("Colas\n"
-                + "1. Insertar\n"
-                + "2. Imprimir elementos\n"
-                + "3. Quitar elemento\n"
-                + "4. Esta llena\n"
-                + "5. Esta vacia\n"
-                + "6. SALIR\n\n"
+                + "1. Crear cola\n"
+                + "2. Insertar\n"
+                + "3. Imprimir elementos\n"
+                + "4. Quitar elemento\n"
+                + "5. Esta llena\n"
+                + "6. Esta vacia\n"
+                + "7. SALIR\n\n"
                 + "Seleccione opcion"));
         switch(opcion){
         
-            case 1:c.insertar();
+            case 1:c.crearCola();
                 break;
-            case 2:c.imprimir();
+            case 2:c.insertar();
                 break;
-            case 3:c.quitar();
+            case 3:c.imprimir();
                 break;
-            case 4:
+            case 4:c.quitar();                
                 break;
-            case 5:
+            case 5:c.llena();
                 break;
-            case 6:
+            case 6:c.vacia();
+                break;
+            case 7:
                 System.exit(0);
             default:
                 
